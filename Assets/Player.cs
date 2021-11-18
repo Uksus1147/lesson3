b0a679e2e9014b3d1670b131973b0c1e592e0f67
Uksus1147 <tihomirovdima028@gmail.com>
2021-11-18 20:43:51 +0300

    Домашнее задание к уроку 3 Тихомирова Дмитрия

---
+++ Assets/Player.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float _speed = 2;
+    private Vector3 _direction;
+    public float Rotatespeed = 100f;
+    public void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void Awake()
+    {
+        _direction = Vector3.zero;
+    }
+    private void Update()
+    {
+        _direction.x = Input.GetAxis("Horizontal");
+        _direction.z = Input.GetAxis("Vertical");
+
+    }
+    private void FixedUpdate()
+    {
+        Move();
+        transform.Rotate(0, Input.GetAxis("Mouse X") * Rotatespeed * Time.fixedDeltaTime, 0);
+    }
+    private void Move()
+    {
+        Vector3 direction = _speed * _direction * Time.fixedDeltaTime;
+        transform.Translate(direction);
+    }
+
+}
